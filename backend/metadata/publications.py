97cd5b70d4f6a6c77238a220a41004c6bb5fc484
Alejandro Velez-Arce <alex@alexv24.com>
2024-12-21 15:01:20 -0500

    move publications to new backend

---
+++ backend/metadata/publications.py
@@ -0,0 +1,119 @@
+from ..model.homepage_publication import HomepagePublication
+
+_PUBLICATIONS = [
+    HomepagePublication(
+        "TDC-2: Multimodal Foundation for Therapeutic Science",
+        "bioRxiv, 2024",
+        2024,
+        6,
+        {
+            "Paper": "https://www.biorxiv.org/content/10.1101/2024.06.12.598655v2"
+        }
+    ),
+    HomepagePublication(
+        "Zero-shot Prediction of Therapeutic Use with Geometric Deep Learning and Clinician Centered Design",
+        "MedRxiv, 2023",
+        2023,
+        3,
+        {
+            "Paper": "https://www.medrxiv.org/content/10.1101/2023.03.19.23287458",
+            "TxGNN Explorer": "http://txgnn.org/"
+        }
+    ),
+    HomepagePublication(
+        "Artificial Intelligence Foundation for Therapeutic Science",
+        "Nature Chemical Biology, 2022",
+        2022,
+        3,
+        {
+            "Paper": "https://www.nature.com/articles/s41589-022-01131-2"
+        }
+    ),
+    HomepagePublication(
+        "Therapeutics Data Commons: Machine Learning Datasets and Tasks for Drug Discovery and Development",
+        "NeurIPS, 2021",
+        2021,
+        11,
+        {
+            "Paper": "https://datasets-benchmarks-proceedings.neurips.cc/paper/2021/hash/4c56ff4ce4aaf9573aa5dff913df997a-Abstract-round1.html",
+            "Poster": "https://drive.google.com/file/d/1LfF8mfPLUqAVEzH3KPBxDO_VF7nLFtiJ/view?usp=sharing"
+        }
+    ),
+    HomepagePublication(
+        "Benchmarking Molecular Machine Learning in Therapeutics Data Commons",
+        "ELLIS ML4Molecules, 2021",
+        2021,
+        8,
+        {
+            "Paper": "https://cloud.ml.jku.at/s/54pB5Eqf6ftX7qA",
+            "Slides": "https://drive.google.com/file/d/1iOSW_5eruca4vdygDxS1H64c49oQuH40/view?usp=sharing"
+        }
+    ),
+    HomepagePublication(
+        "Therapeutics Data Commons: Machine Learning Datasets and Tasks for Drug Discovery and Development",
+        "Baylearn, 2021",
+        2021,
+        6,
+        {
+            "Slides": "https://drive.google.com/file/d/1BNpk3dOdqE3ksgyVV-V3xySdBMq-8cXL/view?usp=sharing",
+            "Poster": "https://drive.google.com/file/d/1LfF8mfPLUqAVEzH3KPBxDO_VF7nLFtiJ/view?usp=sharing"
+        }
+    ),
+    HomepagePublication(
+        "Therapeutics Data Commons",
+        "NSF-Harvard Symposium on Drugs for Future Pandemics, 2020",
+        2020,
+        12,
+        {
+            "Slides": "https://drive.google.com/file/d/11eTrh_lsqPcwu3RZRYjJGNpJ3s18YlBS/view",
+            "Video": "https://harvard.zoom.us/rec/share/HO0TjRPs56YG-Fu3i033izaTwebB4KwUhPeNURkWSI-anrH9su03lCtUlHeZG-WP.67ZJmAIHsD7Q_2GQ",
+            "#futuretx20": "https://www.drugsymposium.org/"
+        }
+    ),
+    HomepagePublication(
+        "Machine Learning to Translate the Cancer Genome and Epigenome Session",
+        "AACR Annual Meeting",
+        2022,
+        2,
+        {
+            "Meeting": "https://www.aacr.org/meeting/aacr-annual-meeting-2022/"
+        }
+
+    ),
+    HomepagePublication(
+        "Few-Shot Learning for Network Biology",
+        "KDD Workshop on Data Mining in Bioinformatics",
+        2021,
+        1,
+        {
+            "keynote": "https://biokdd.org/biokdd21/keynote.html"
+        }
+    ),
+    HomepagePublication(
+        "Actionable machine learning for drug discovery and development",
+        "Broad Institute, Models, Inference & Algorithms Seminar",
+        2021,
+        0,
+        {
+            "Talk": "https://www.broadinstitute.org/talks/actionable-machine-learning-drug-discovery-and-development"
+        }
+    ),
+    HomepagePublication(
+        "Graph Neural Networks for Biomedical Data",
+        "Machine Learning in Computational Biology",
+        2020,
+        2,
+        {
+            "Schedule": "https://sites.google.com/cs.washington.edu/mlcb2020/schedule?authuser=0"
+        }
+    ),
+    HomepagePublication(
+        "Graph Neural Networks for Identifying COVID-19 Drug Repurposing Opportunities",
+        "MIT AI Cures",
+        2020,
+        1,
+        {
+            "Webpage": "https://www.aicures.mit.edu/drugdiscoveryconference"
+        }
+    )
+]
