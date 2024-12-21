915109c4182ac773a229db17d23a64a7016e6925
Alejandro Velez-Arce <alex@alexv24.com>
2024-12-21 15:39:09 -0500

    add new presentations and modify txgnn

--- backend/metadata/publications.py
+++ backend/metadata/publications.py
@@ -2,21 +2,42 @@ from ..model.homepage_publication import HomepagePublication
 
 _PUBLICATIONS = [
     HomepagePublication(
+        "Signals in the Cells: Multimodal and Contextualized Machine Learning Foundations for Therapeutics",
+        "(Spotlight) NeurIPS 2024 Workshop on AI for New Drug Modalities",
+        2024,
+        12,
+        {
+            "Paper": "https://openreview.net/pdf?id=kL8dlYp6IM",
+            "Poster": "https://drive.google.com/file/d/1plypydZCaegbgxyCl-xehFxSgwX6e8So/view?usp=sharing"
+        }
+    ),
+    HomepagePublication(
+        "(Seminar) Signals in the Cells: Multimodal and Contextualized Machine Learning Foundations for Therapeutics",
+        "Western Bioinformatics Seminar Series: Alejandro Velez-Arce",
+        2024,
+        11,
+        {
+            "Event": "https://www.events.westernu.ca/events/schulich-medicine-dentistry/2024-11/western-bioinformatics-nov14.html",
+        }
+    ),
+    HomepagePublication(
         "TDC-2: Multimodal Foundation for Therapeutic Science",
-        "bioRxiv, 2024",
+        "Molecular Machine Learning Conference (MoML2024). Hosted at Mila Agora on June 19th",
         2024,
         6,
         {
-            "Paper": "https://www.biorxiv.org/content/10.1101/2024.06.12.598655v2"
+            "Paper": "https://www.biorxiv.org/content/10.1101/2024.06.12.598655v2",
+            "Conference": "https://portal.ml4dd.com/moml-2024",
+            "Poster and Tweet": "https://x.com/ProjectTDC/status/1803581129376629234"
         }
     ),
     HomepagePublication(
         "Zero-shot Prediction of Therapeutic Use with Geometric Deep Learning and Clinician Centered Design",
-        "MedRxiv, 2023",
-        2023,
+        "Nature Medicine, 2024",
+        2024,
         3,
         {
-            "Paper": "https://www.medrxiv.org/content/10.1101/2023.03.19.23287458",
+            "Paper": "https://www.nature.com/articles/s41591-024-03233-x",
             "TxGNN Explorer": "http://txgnn.org/"
         }
     ),
